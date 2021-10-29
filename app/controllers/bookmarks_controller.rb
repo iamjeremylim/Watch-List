489088b2ea5f74e2c0a878abd9764cf0eca711b9
Jeremy Lim <limjeremy29@gmail.com>
2021-10-29 20:13:55 +0800

    update code for creating/deleting bookmark and styling of list index/show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,5 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
@@ -8,7 +7,7 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    @list = @bookmark.list
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -24,14 +23,10 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 
   def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
-  def set_list
-    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:list_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: :destroy
+  end
+
   resources :bookmarks, only: [:index, :new, :create]
-  resources :bookmarks, only: :destroy
 end
